ad1f0cc824c3ef84520de582341761bb2a09e9ff
woolimi <woolim.park@hotmail.com>
2018-12-17 21:48:58 +0100

    computer AI added

--- ttc.js
+++ ttc.js
@@ -1,103 +1,175 @@
-let body = document.body;
-let tb = document.createElement('table');
-body.appendChild(tb);
-let retryBtn = document.createElement('button');
-retryBtn.textContent = 'RETRY ?';
-retryBtn.addEventListener('click', function(){
-    boxes.forEach(function(row){
-        row.forEach(function(val){
-            val.textContent = '';
-            val.addEventListener('click', boxClick);
-        });
-    });
-    winner.textContent = '';
-});
-body.appendChild(retryBtn);
-let winner = document.createElement('h1');
-body.appendChild(winner);
+const tbody = document.querySelector('#tbody')
+const tr = tbody.children
+const retryBtn = document.querySelector('#retryBtn')
+retryBtn.addEventListener('click', reset)
+const winner = document.querySelector('#winner')
+let turn = 'X'
 
-let boxes = []; //make empty array to fill 3x3 boxes
-let rows = [];
-let turn = 'X';
+// initialize game
+reset()
 
-//make table elements
-for(let i=0; i<3;i++){
-    let tr = document.createElement('tr');
-    let row = [];
-    tb.appendChild(tr);
-    boxes.push(row);
-    rows.push(tr);
-    for(let j=0; j<3;j++){
-        let td = document.createElement('td');
-        td.addEventListener('click', boxClick);
-        tr.appendChild(td);
-        row.push(td);
-    }
-}
-
-function boxClick(e){
-    if(e.target.textContent === ''){
-        let nbRow = rows.indexOf(e.target.parentNode);
-        let nbCol = boxes[nbRow].indexOf(e.target);
-        console.log(`row : ${nbRow}, col : ${nbCol}`);
-
-        e.target.textContent = turn;
-        if(checkWin(turn)){
-            //remove all eventListener
-            removeEvents();
-            // massage for winner
-            winner.textContent = `Player ${turn} WIN !`
-            //initialize first player
-            turn = 'X';
-        } else{
-            turnChange(turn);
-        }
+function boxClick (e) {
+  if (e.currentTarget.textContent || turn === 'O') {
+    return
+  }
+  if (e.currentTarget.textContent === '') {
+    e.target.textContent = 'X'
+    turnChange()
+    if (checkWin('X')) {
+      // remove all eventListener
+      removeEvents()
+      // massage for winner
+      winner.textContent = `Player WIN !`
     } else {
-        console.log('already clicked');
+      // computer play after 0.5sec
+      setTimeout(computer, 500)
     }
+  }
 }
 
-function turnChange(t){
-    if(t === 'X') return turn = 'O';
-    else return turn = 'X';
+function turnChange () {
+  (turn === 'X') ? turn = 'O' : turn = 'X'
 }
 
-function checkWin(t){
-    let result = false;
-    //check row
-    for(let i=0; i<3; i++){
-        if(
-            boxes[i][0].textContent === t &&
-            boxes[i][1].textContent === t &&
-            boxes[i][2].textContent === t
-        ) result = true;
-    }
-    //check col
-    for(let i=0; i<3; i++){
-        if(
-            boxes[0][i].textContent === t &&
-            boxes[1][i].textContent === t &&
-            boxes[2][i].textContent === t
-        ) result = true;
-    }
-    //check diagonal
-    if(
-        boxes[0][0].textContent === t &&
-        boxes[1][1].textContent === t &&
-        boxes[2][2].textContent === t
-    ) result = true;
-    if(
-        boxes[0][2].textContent === t &&
-        boxes[1][1].textContent ===t &&
-        boxes[2][0].textContent === t
-    ) result = true;
-    return result;
+function checkWin (t) {
+  let result = false
+  // check row
+  for (let i = 0; i < 3; i++) {
+    if (
+      tr[i].children[0].textContent === t &&
+      tr[i].children[1].textContent === t &&
+      tr[i].children[2].textContent === t
+    ) result = true
+  }
+  // check col
+  for (let i = 0; i < 3; i++) {
+    if (
+      tr[0].children[i].textContent === t &&
+      tr[1].children[i].textContent === t &&
+      tr[2].children[i].textContent === t
+    ) result = true
+  }
+  // check diagonal
+  if (
+    tr[0].children[0].textContent === t &&
+    tr[1].children[1].textContent === t &&
+    tr[2].children[2].textContent === t
+  ) result = true
+  if (
+    tr[0].children[2].textContent === t &&
+    tr[1].children[1].textContent === t &&
+    tr[2].children[0].textContent === t
+  ) result = true
+  return result
 }
 
-function removeEvents() {
-    for(let i = 0; i<3; i++){
-        for(let j=0; j<3; j++){
-            boxes[i][j].removeEventListener('click', boxClick);
-        }
+function removeEvents () {
+  for (let i = 0; i < 3; i++) {
+    for (let j = 0; j < 3; j++) {
+      tbody.children[i].children[j].removeEventListener('click', boxClick)
     }
+  }
+}
+
+function reset () {
+  for (let i = 0; i < 3; i++) {
+    for (let j = 0; j < 3; j++) {
+      tr[i].children[j].addEventListener('click', function (e) {
+        boxClick(e)
+      })
+      tr[i].children[j].textContent = ''
+    }
+  }
+  turn = 'X'
+  winner.textContent = ''
+}
+
+// turn O
+function computer () {
+  if (!fillBlank()) {
+    winner.textContent = `DRAW`
+  }
+  // check winner
+  if (checkWin(turn)) {
+    // remove all eventListener
+    removeEvents()
+    // massage for winner
+    winner.textContent = `Computer WIN !`
+  } else {
+    // change back to user
+    turnChange()
+  }
+}
+
+function fillBlank () {
+  // no blank, no fill
+  const board = []
+  for (let i = 0; i < 3; i++) {
+    for (let j = 0; j < 3; j++) {
+      board.push(tr[i].children[j])
+    }
+  }
+  if (board.filter(v => !v.textContent).length === 0) {
+    return false
+  }
+
+  // center
+  if (!tr[1].children[1].textContent) {
+    tr[1].children[1].textContent = 'O'
+    return true
+  }
+
+  // 중앙을 거쳐가는 대각선, 십자가 -> 중앙을 안거치는 가로 2 세로 2 체크
+  const checkList = [
+    // 대각선
+    [tr[0].children[0], tr[1].children[1], tr[2].children[2]],
+    [tr[0].children[2], tr[1].children[1], tr[2].children[0]],
+    // 십자가
+    [tr[1].children[0], tr[1].children[1], tr[1].children[2]],
+    [tr[0].children[1], tr[1].children[1], tr[2].children[1]],
+    // 가로 2
+    [tr[0].children[0], tr[0].children[1], tr[0].children[2]],
+    [tr[2].children[0], tr[2].children[1], tr[2].children[2]],
+    // 세로 2
+    [tr[0].children[0], tr[1].children[0], tr[2].children[0]],
+    [tr[0].children[2], tr[1].children[2], tr[2].children[2]]
+  ]
+
+  // 선 공격
+  for (let i = 0; i < checkList.length; i++) {
+    if (
+      checkList[i].filter(v => v.textContent === 'O').length === 2 &&
+      checkList[i].filter(v => !v.textContent).length === 1
+    ) {
+      checkList[i].filter(v => !v.textContent)[0].textContent = 'O'
+      return true
+    }
+  }
+
+  // 후 수비
+  for (let i = 0; i < checkList.length; i++) {
+    if (
+      checkList[i].filter(v => v.textContent === 'X').length === 2 &&
+      checkList[i].filter(v => !v.textContent).length === 1
+    ) {
+      checkList[i].filter(v => !v.textContent)[0].textContent = 'O'
+      return true
+    }
+  }
+
+  // 센터자리가 이미 차있고, 수비도 공격도 할 곳이 없고 모서리가 비어있으면 모서리에 둔다
+  if (
+    tr[1].children[1].textContent &&
+    [board[0].textContent, board[2].textContent, board[6].textContent, board[8].textContent].includes('')
+  ) {
+    const edges = [board[0], board[2], board[6], board[8]]
+    const emptyEdges = edges.filter(v => !v.textContent)
+    emptyEdges.splice(Math.floor(Math.random() * emptyEdges.length), 1)[0].textContent = 'O'
+    return true
+  }
+
+  // 대각선 조차도 막혔을 경우 랜덤으로 둔다
+  const emptyBoard = board.filter(v => !v.textContent)
+  emptyBoard.splice(Math.floor(Math.random() * emptyBoard.length), 1)[0].textContent = 'O'
+  return true
 }
